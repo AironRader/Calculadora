6308255a079c212f531723ac2f6d9b66c936cc4a
AironRader <37662874+AironRader@users.noreply.github.com>
2018-03-27 09:10:15 -0300

    Add files via upload

---
+++ Calculadora.php
@@ -0,0 +1,54 @@
+ <?php
+        $num1 = isset($_POST["numero1"])?$_POST["numero1"]:"";
+        $num2 = isset($_POST["numero2"])?$_POST["numero2"]:"";
+        $som = isset($_POST["Somar"])? $_POST["Somar"]:"";
+        $sub = isset($_POST["Subtrair"])? $_POST["Subtrair"]:"";
+        $mult = isset($_POST["Multiplicar"])? $_POST["Multiplicar"]:"";
+        $div = isset($_POST["Dividir"])? $_POST["Dividir"]:"";
+       function validar($num1,$num2){
+          return is_numeric($num1) && is_numeric($num2);
+       }
+        function somar($num1,$num2){
+            $total = $num1 + $num2;
+            echo "Resultado: $total";
+        }
+        function subtrair($num1,$num2){
+            $total = $num1 - $num2;
+            echo "Resultado: $total";  
+        }
+        function multiplicar($num1,$num2){
+            $total = $num1 * $num2;
+            echo "Resultado: $total";
+        }
+        function dividir($num1,$num2){
+             $total = $num1 /$num2;
+            echo "Resultado: $total";
+        }
+
+        if ($som){
+           if(validar($num1,$num2)){ 
+               somar($num1,$num2);
+           }else{
+               echo "Valor não encontrado";
+           }
+        }else if($sub){
+            if(validar($num1,$num2)){ 
+                 subtrair($num1,$num2);
+                }else{
+                    echo "Valor não encontrado";
+                }
+        }else if($mult){
+            if(validar($num1,$num2)){ 
+                multiplicar($num1,$num2);
+            }else{
+                echo "Valor não encontrado";
+            }
+        }else if ($div){
+            if(validar($num1,$num2)){
+            dividir($num1,$num2);
+        }else{
+            echo "Valor não encontrado";
+        }
+        }
+?>
+<br/><html><a href = "Calculadora.html">Voltar</a></html>
